6c7565eb0e0324e921ddb91e3b4b53cc3f4f9754
amyzhao3969 <zhao_yiming@hotmail.com>
2021-07-07 18:57:33 -0400

    initial commit

---
+++ src/main/java/com/example/demo/TrackerWithUserAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerWithUserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerWithUserAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/amyzhao3969/tracker/with/user/authentication/TrackerWithUserAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.amyzhao3969.tracker.with.user.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerWithUserAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
